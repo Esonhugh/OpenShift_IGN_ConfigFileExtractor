a818463ccdfbc89a7f14726f1435b8706de266f8
Esonhugh <esonhughoutside@gmail.com>
2023-01-12 23:10:49 +0800

    init project

---
+++ ign_file_secret_extrator.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+export FILENAME="ign_bootstrap.json"
+export RESULT="configs.mixed"
+
+export CONFIG_WITH_BASE64_URL_FORMAT=configs.base64
+export CONFIG_WITH_BASE64_EACH_LINE=configs.eachline.base64
+
+jqed_jsonfile() {
+    cat $FILENAME|jq '.storage.files[]'|jq '.contents.source' > $CONFIG_WITH_BASE64_URL_FORMAT
+}
+
+parse_jsons() {
+    cat $CONFIG_WITH_BASE64_URL_FORMAT|cut -d ',' -f 2 |sed -e "s/\"//g" > $CONFIG_WITH_BASE64_EACH_LINE
+}
+
+banners() {
+    echo "===============[CONFIG]================"
+}
+
+parse_base64() {
+    while read line; do
+        # if line is empty then skip
+        if [ -z "$line" ]; then
+            continue
+        fi
+        banners >> $RESULT
+        echo $line|base64 -d >> $RESULT
+    done < $CONFIG_WITH_BASE64_EACH_LINE 
+}
+
+if [ -f $FILENAME ]; then
+    jqed_jsonfile
+    parse_jsons
+    parse_base64
+else
+    echo "File not specificed or not found."
+fi
+
